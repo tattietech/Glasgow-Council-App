9cd8227110bd12ef709ecc15689a338c5f25f1ca
Andrew Bruce <abruce1711@gmail.com>
2018-02-06 14:11:04 +0000

    Got the comment layout working on attraction page

--- app/src/main/java/me/andrewbruce/assessment3/Attraction1.java
+++ app/src/main/java/me/andrewbruce/assessment3/Attraction1.java
@@ -3,9 +3,12 @@ package me.andrewbruce.assessment3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.TimeZone;
 
 public class Attraction1 extends AppCompatActivity {
@@ -14,18 +17,25 @@ public class Attraction1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attraction1);
+
         ArrayList<Comment> al = new ArrayList<>();
 
-        for(int i=0; i<10;i++) {
-            Comment comment = new Comment("Username", "This is the comments main body " + i);
+        CommentAdapter commentAdapter = new CommentAdapter(this, al);
+        ListView lstComments = (ListView)findViewById(R.id.lstComments);
+
+        Date date = new Date();
+        String modifiedDate = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(date);
+
+        int i;
+        for(i=0; i<4;i++) {
+            Comment comment = new Comment("Username", "This is the comments main body " + i + " ", modifiedDate);
             al.add(comment);
         }
 
-        CommentAdapter commentAdapter = new CommentAdapter(this, al);
+        lstComments.getLayoutParams().height = 200 * (i);
+        lstComments.requestLayout();
+        lstComments.setAdapter(commentAdapter);;
+        lstComments.setScrollContainer(false);
 
-
-        ListView lstComments = (ListView)findViewById(R.id.lstComments);
-        lstComments.setAdapter(commentAdapter);
-      //  lstComments.setScrollContainer(false);
     }
 }
--- app/src/main/java/me/andrewbruce/assessment3/Comment.java
+++ app/src/main/java/me/andrewbruce/assessment3/Comment.java
@@ -1,13 +1,17 @@
 package me.andrewbruce.assessment3;
 
 
+import java.util.Date;
+
 public class Comment {
     private String username;
     private String comment;
+    private String date;
 
-    public Comment(String username, String comment) {
+    public Comment(String username, String comment, String date) {
         this.username = username;
         this.comment = comment;
+        this.date = date;
     }
 
     public String getUsername() {
@@ -16,6 +20,10 @@ public class Comment {
 
     public String getComment() {
         return comment;
+    }
+
+    public String getDate() {
+        return date;
     }
 
 
